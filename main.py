e484686974be2fc4bbdc038c7805fbdfed459feb
Taotao Pan <pantaotao@icloud.com>
2020-06-17 15:30:47 +1000

    [UPDATE] application for link_prediction algorithms

--- main.py
+++ main.py
@@ -1,78 +1,47 @@
-from netwalk.utils.reader import FileReader
-from netwalk.utils.graph import UndiGraph, DiGraph
-from netwalk.algorithms.centrality.pageRank import PageRank
-from netwalk.algorithms.centrality.brandes import Brandes
-# import matplotlib.pyplot as plt
-from typing import (
-    Dict,
-    List,
-    Tuple,
-    Union
+#
+# The entry place to demonstrate different applications
+# Where, applications were encapsulated into samples folder.
+#
+# @Author: Terry Pan
+# @Date: Wed Jun 17 2020
+# @Email: pttdev123@gmail.com
+# @Last modified by: Terry Pan
+# @Last modified time: Wed Jun 17 2020 3:28:40 PM
+#
+import os
+from netwalk.utils.misc import (
+    file_to_graph,
+    get_dataset_from_file
 )
-import networkx as nx
-
-
-def format_print(weights: Union[List[str], List[Tuple[str, float]]]):
-    title = ("node", "weight")
-    print(f'|{title[0]:5}|\t{title[1]:8}|')
-    print('|-------------|-------------|')
-    for k, v in weights:
-        print(f'|{k:5} | {v:12} |')
-    print()
-
-
-def page_rank_application(file_loc: str) \
-        -> Tuple[Dict[str, float], Union[List[str], List[Tuple[str, float]]]]:
-    txt_reader = FileReader(file_loc)
-    graph = UndiGraph()
-    # build undirected graph
-    for record in txt_reader.get_data():
-        graph.add_edge((record[0], record[1]))
-
-    page_rank = PageRank(graph)
-    weights = page_rank.run_page_rank_algorithm()
-    top_10_central_nodes = page_rank.get_top_n_centrality_nodes(
-        weights, 10, False)
-    return weights, top_10_central_nodes
-
-
-def betweenness_application(file_loc: str) \
-        -> Tuple[Dict[str, float], Union[List[str], List[Tuple[str, float]]]]:
-    txt_reader = FileReader(file_loc)
-    graph = UndiGraph()
-    # build undirected graph
-    for record in txt_reader.get_data():
-        graph.add_edge((record[0], record[1]))
-    b = Brandes(graph)
-    b.run_brandes_algorithm()
-    weights = b.get_betweenness()
-    top_10_betweenness_nodes = b.get_top_n_betweenness(weights, 10, False)
-    return weights, top_10_betweenness_nodes
-
-
-def networkx_pagerank(file_loc: str):
-    txt_reader = FileReader(file_loc)
-    graph = nx.Graph()
-    # build undirected graph
-    for record in txt_reader.get_data():
-        graph.add_edge(*record)
-
-    res = nx.pagerank(graph)
-
-    fin = sorted(res.items(), key=lambda i: i[1], reverse=True)[:10]
-    format_print(fin)
+from netwalk.utils.graph import Graph
+from netwalk.samples.link_prediction_samples.deep_walk import (
+    deep_walk_application
+)
+from netwalk.samples.link_prediction_samples.neighborhood_based import (
+    neighbourhood_application
+)
+from typing import List, Tuple
 
 
 if __name__ == "__main__":
-    file_loc = "./asset/data.txt"
+    # the project root location
+    project_root: str = os.path.dirname(os.path.abspath(__file__))
 
-    weights, top_10 = betweenness_application(file_loc)
+    # locations for different datasets
+    assets_loc: str = "assets"
+    train_file: str = os.path.join(project_root, assets_loc, "training.txt")
+    neg: str = os.path.join(project_root, assets_loc, "val_negative.txt")
+    pos: str = os.path.join(project_root, assets_loc, "val_positive.txt")
+    test: str = os.path.join(project_root, assets_loc, "test.txt")
 
-    for node in top_10:
-        print(node, end=" ")
+    # create training, validation, testing dataset in desired format
+    train_set: Graph = file_to_graph(train_file)
+    validate_set: List[Tuple[str, str, str]] = get_dataset_from_file(neg, '0')\
+        + get_dataset_from_file(pos, '1')
+    test_set = get_dataset_from_file(test)
+
+    print("Neighbourhood based methods")
+    neighbourhood_application(train_set, validate_set)
     print()
-
-    weights, top_10 = page_rank_application(file_loc)
-    for node in top_10:
-        print(node, end=" ")
-    print()
+    print("Deep walk")
+    deep_walk_application(train_set, validate_set, test_set)
