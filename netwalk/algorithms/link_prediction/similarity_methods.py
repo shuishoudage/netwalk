9899495339318e4f4f0f5852f0fe1adf6ba5c5d0
Taotao Pan <pantaotao@icloud.com>
2020-06-17 16:48:14 +1000

    [ADD] enum of similarity methods

---
+++ netwalk/algorithms/link_prediction/similarity_methods.py
@@ -0,0 +1,16 @@
+#
+# Enum of different measurements
+#
+# @Author: Terry Pan
+# @Date: Wed Jun 17 2020
+# @Email: pttdev123@gmail.com
+# @Last modified by: Terry Pan
+# @Last modified time: Wed Jun 17 2020 4:44:03 PM
+#
+import enum
+
+
+class Measure(enum.Enum):
+    JACCARD = 1
+    ADAR = 2
+    PREFERENTIAL = 3
